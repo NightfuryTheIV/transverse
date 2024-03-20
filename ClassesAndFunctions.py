be73fe66284fae472010f66309fb483b10341a5a
Nightfury <goldfury.exe@gmail.com>
2024-03-20 14:38:10 +0100

    test de la classe button

--- ClassesAndFunctions.py
+++ ClassesAndFunctions.py
@@ -1,71 +1,59 @@
-from math import sin, cos, pi, radians, degrees
 import pygame
-
-class Character:
-    def __init__(self, name, speed, x_pos, y_pos):
-        self.name = name
-        self.speed = speed
-        self.x = x_pos
-        self.y = y_pos
-        self.angle = 0
-
-    def __str__(self):
-        return f"{self.name}: {self.speed} m/s of speed. Currently at ({self.x}; {self.y}) position"
-
-    def movement(self, friction, gravity): # friction et gravité de la map
-        while True: # faudra changer quand on aura construit quelque chose avec des events
-            velocity = 0 
-            for event in pygame.event.get():
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        velocity += self.speed
-                    if event.key == pygame.K_LEFT:
-                        velocity -= self.speed
-                    if event.key == pygame.K_UP:
-                        for i in range(0, 100):
-                            t = i/10
-                            self.y = -1/2 * gravity * t**2 + sin(pi/3) * velocity + self.y
-                    
-                    velocity *= friction
-                    self.x += velocity
-                
-                
-
-
-class Bullet:
-    def __init__(self, x_pos, y_pos, bullet_name, weight):
-        self.x = x_pos
-        self.y = y_pos
-        self.bullet = bullet_name
-        self.weight = weight
-
-    def fire(self, initial_pos, angle, gravity): # initial position et angle du joueur, gravité de la map
-        while True: # pareil, faudra faire un truc avec les events
-            radians = radians(angle)
-            for i in range(0, 100):
-                t = i/10
-                self.y = -0.5 * gravity * t**2 + sin(radians) * 1 + initial_pos # va falloir voir pour v0 ici
-                self.x = cos(radians) * t
-
-
-class Map:
-    def __init__(self, map_name, friction, gravity):
-        self.map = map_name
-        self.friction = friction
-        self.gravity = gravity
+import sys
 
 
 class Button:
-    def __init__(self, command, fontsize, text = "Click!", pos=[0, 0], color = (255, 255, 255)):
-        self.command = command
-        self.fontsize = fontsize
+    def __init__(self, x, y,  length, height, font_size, text, r, g, b):
+        self.length = length
+        self.height = height
         self.text = text
-        self.pos = pos
-        self.color = color
+        self.rect = self
+        self.color = (r, g, b)
+        self.font_size = font_size
 
+        self.surface = pygame.Surface((length, height))
 
-    def clicked(self):
-        if self.rect.collidepoint(pygame.mouse.get_pos()):
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.command()
+        font = pygame.font.Font(None, self.font_size)
+        text_act = font.render(self.text, True, self.color)
+        text_rect = text_act.get_rect(center=(self.surface.get_width() / 2, self.surface.get_height() / 2))
+        self.button_rect = pygame.Rect(x, y, length, height)  # Adjust the position as needed
+
+        # Shwo the button text
+        self.surface.blit(text_act, text_rect)
+
+        # Draw the button on the screen
+        screen.blit(self.surface, (self.button_rect.x, self.button_rect.y))
+
+    def pressed_check(self):
+        clock.tick(60)
+        screen.fill((0, 200, 255))
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if self.button_rect.collidepoint(event.pos):
+                    print("YOOOOOOOOOOOOOOOOOOO")
+
+        if self.button_rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(self.surface, (127, 255, 212), (1, 1, 148, 48))
+        else:
+            pygame.draw.rect(self.surface, (0, 0, 0), (0, 0, 150, 50))
+            pygame.draw.rect(self.surface, (255, 255, 255), (1, 1, 148, 48))
+            pygame.draw.rect(self.surface, (0, 0, 0), (1, 1, 148, 1), 2)
+            pygame.draw.rect(self.surface, (0, 100, 0), (1, 48, 148, 10), 2)
+
+        pygame.display.update()
+
+# Initialize Pygame
+pygame.init()
+
+clock = pygame.time.Clock()
+
+# Create a Pygame window
+window_size = (400, 400)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption('Pygame Clickable Button')
+
+buttonss = Button(10, 10, 250, 150, 25, "hee hee", 0, 255, 255)
+while True:
+    buttonss.pressed_check()
+
