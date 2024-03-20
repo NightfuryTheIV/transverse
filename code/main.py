7de2884531761b4f8be74e54fa346e7e2cf456f8
Spoken-sama <151039597+Spoken-sama@users.noreply.github.com>
2024-03-20 16:54:17 +0100

    full menu

--- code/main.py
+++ code/main.py
@@ -7,13 +7,15 @@ pygame.init()
 #All necessaries
 technoblade = technoblade()
 character = Player()
+Title = pygame.image.load('../image/TITLE.png')
 menu = pygame.image.load('../image/menu.png')
 background = pygame.image.load('../image/Fond.png')
 pygame.display.set_caption("Technoblade Trinity")
 screen = pygame.display.set_mode((1280, 720))
 running = True
 scale=1.7
-scale2=1.5
+scale2=1
+scale3=0.7
 
 clock = pygame.time.Clock()  # Creating a clock object for controlling the frame rate
 frame_rate = 10  # Set your desired frame rate here
@@ -25,10 +27,14 @@ def zoomimg(scale):
 
 def zoomimg2(scale):
     Menu_im = pygame.transform.scale(menu, (int(menu.get_width()*scale2),int(menu.get_height()*scale2)))
-    screen.blit(Menu_im, (475, 100))
+    screen.blit(Menu_im, (525, 250))
     pygame.display.flip()
     clock.tick(frame_rate)
-
+def zoomimg3(scale):
+    Title_im = pygame.transform.scale(Title, (int(Title.get_width()*scale3),int(Title.get_height()*scale3)))
+    screen.blit(Title_im, (360, 0))
+    pygame.display.flip()
+    clock.tick(frame_rate)
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -46,6 +52,7 @@ while running:
     screen.blit(technoblade.player.image, technoblade.player.rect)
     zoomimg(scale)
     zoomimg2(scale2)
+    zoomimg3(scale3)
 
 
     if technoblade.pressed.get(pygame.K_d):
